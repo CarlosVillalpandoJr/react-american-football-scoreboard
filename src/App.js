29df26f348d0e23d1ad02e2eb811e67dbcce66ab
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-07 14:32:36 -0500

    added a home/away touchdown incrementor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
+  const addHomeTouchdown = () => setHomeScore(homeScore + 7);
+  const addAwayTouchdown = () => setAwayScore(awayScore + 7);
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,11 +33,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={addHomeTouchdown}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={addAwayTouchdown}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
